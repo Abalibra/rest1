7a44e8bd2f9eb0092c9deb80b15ef7a94e9f3d27
abalibra <abalibra@DESKTOP-A7D4699>
2024-01-22 22:45:00 +0300

    updating imports and maven

--- rest1/src/main/java/RestFiles/s2.java
+++ rest1/src/main/java/RestFiles/s2.java
@@ -1,10 +1,18 @@
 package RestFiles;
 
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+
 public class s2 {
 
+	@Test
+	public void t() {}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("PC32");
+		System.out.print("PC322");
 	}
 
 }
